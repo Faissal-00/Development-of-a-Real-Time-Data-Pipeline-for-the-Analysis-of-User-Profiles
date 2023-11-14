8e180c75cd4655ff56eb5218020b82ffb5ca3a54
Faissal-00 <132353836+Faissal-00@users.noreply.github.com>
2023-11-14 11:17:25 +0100

    Consumer.py

--- Consumer.py
+++ Consumer.py
@@ -1,16 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[3]:
-
-
-#!/usr/bin/env python
-# coding: utf-8
-
 from pyspark.sql import SparkSession
-from pyspark.sql.functions import from_json, col, to_date, concat_ws, current_date, datediff, year
+from pyspark.sql.functions import from_json, col, to_date, concat_ws, current_date, datediff, year, count, avg, split, expr, desc
 from pyspark.sql.types import StructType, StructField, StringType, IntegerType
 from cassandra.cluster import Cluster
+from pymongo import MongoClient
 
 # Initialize Spark session
 spark = SparkSession.builder\
@@ -66,40 +61,69 @@ parsedStreamDF = kafka_source.selectExpr("CAST(value AS STRING) as json") \
     .select(from_json(col("json"), json_schema).alias("data")) \
     .select("data.*")
 
-# 1. Build Full Name
-parsedStreamDF = parsedStreamDF.withColumn("full_name", concat_ws(" ", "name.title", "name.first", "name.last"))
 
-# 2. Calculate Age
-parsedStreamDF = parsedStreamDF.withColumn("calculated_age", year(current_date()) - year(to_date("dob.date")))
+# 1. Transformations
+parsedStreamDF = parsedStreamDF.withColumn("full_name", 
+    concat_ws(" ", 
+        col("name.title"), 
+        col("name.first"), 
+        col("name.last")
+    )
+)
+parsedStreamDF = parsedStreamDF.withColumn("calculated_age", year(current_date()) - year(to_date(parsedStreamDF["dob.date"])))
+parsedStreamDF = parsedStreamDF.withColumn("complete_address", 
+    concat_ws(", ", 
+        col("location.street.number").cast("string"), 
+        col("location.street.name"), 
+        col("location.city"), 
+        col("location.state"), 
+        col("location.country"), 
+        col("location.postcode").cast("string")
+    )
+)
 
-# 3. Build Complete Address
-parsedStreamDF = parsedStreamDF.withColumn("complete_address", concat_ws(", ", "location.street.number", "location.street.name", "location.city", "location.state", "location.country", "location.postcode"))
-
-# 4. Delete the old columns
-parsedStreamDF = parsedStreamDF.drop("name.title", "name.first", "name.last", "location.street.number", "location.street.name", "location.city", "location.state", "location.country", "location.postcode")
-
-# # Define the Cassandra cluster
+# Define the Cassandra cluster
 cluster = Cluster(['localhost'],port=9042)
 session = cluster.connect()
 
-# print(session)
 # Define the keyspace name
 keyspace = "user_profiles"
 
 # Define the table name
-table_name = "Users"
+table_name = "users"
+
+# Create keyspace and table if they don't exist
+session.execute(f"CREATE KEYSPACE IF NOT EXISTS {keyspace} WITH REPLICATION = {{'class': 'SimpleStrategy', 'replication_factor': 1}}")
+session.execute(f"USE {keyspace}")
+
+# Define the table with full_name as the primary key
+table_creation_query = f"""
+    CREATE TABLE IF NOT EXISTS {table_name} (
+        full_name TEXT PRIMARY KEY,
+        calculated_age INT,
+        complete_address TEXT
+    )
+"""
+session.execute(table_creation_query)
+
+# Select only the columns needed for Cassandra table
+cassandraDF = parsedStreamDF.select("full_name", "calculated_age", "complete_address")
+
 
 # Function to save DataFrame to Cassandra
-def save_to_cassandra(df, keyspace, table_name):
-    # Save the DataFrame to Cassandra
-    df.writeStream \
-        .format("org.apache.spark.sql.cassandra") \
-        .outputMode("append") \
-        .options(keyspace=keyspace)\
-        .option(table=table_name) \
-        .save()
+def save_to_cassandra(df, keyspace, table_name, checkpoint_location):
+    query = df.writeStream \
+        .foreachBatch(lambda batch_df, batch_id: batch_df.write \
+                      .format("org.apache.spark.sql.cassandra") \
+                      .mode("append") \
+                      .option("keyspace", keyspace) \
+                      .option("table", table_name) \
+                      .option("checkpointLocation", checkpoint_location) \
+                      .save()) 
+    return query
 
-# Define the streaming query
-query = parsedStreamDF.writeStream.format('console').start()
-query.awaitTermination()
 
+# Start the streaming query
+checkpoint_location = "./checkpoint/data"
+query = save_to_cassandra(cassandraDF, keyspace, table_name, checkpoint_location)
+query.start().awaitTermination()
